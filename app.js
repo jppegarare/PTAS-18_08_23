cf37b73b2d609fe4a771d163ba87a1cfcd84c0d4
João Pegarare <jppegarare18@gmail.com>
2023-10-19 14:15:05 -0400

    corrigindo trabalho e começando a proxima atividade

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+const produtos = [
+  {id: 1, nome: "Bolo de Chocolate", preco: "R$ 50,00", descricao: "descricao", imagem: "/chocolate.png"},
+  {id: 2, nome: "Bolo de Limão", preco: "R$ 40,00", descricao: "descricao", imagem: "/limao.png"},
+  {id: 3, nome: "Bolo de Cenoura", preco: "R$ 35,00", descricao: "descricao", imagem: "/chocolate.png"},
+  {id: 4, nome: "Bolo de Maracuja", preco: "R$ 25,00", descricao: "descricao", imagem: "/chocolate.png"},
+  {id: 5, nome: "Bolo Vulcão Leite Ninho e Goiabada", preco: "R$ 45,00", descricao: "descricao", imagem: "/chocolate.png"},
+  {id: 6, nome: "Bolo de Milho", preco: "R$ 20,00", descricao: "descricao", imagem: "/chocolate.png"},
+  {id: 7, nome: "Bolo de Banana", preco: "R$ 25,00", descricao: "descricao", imagem: "/chocolate.png"},
+  {id: 8, nome: "Bolo de Laranja", preco: "R$ 35,00", descricao: "descricao", imagem: "/chocolate.png"},
+  {id: 9, nome: "Bolo de Abacaxi", preco: "R$ 30,00", descricao: "descricao", imagem: "/chocolate.png"},
+  {id: 10, nome: "nome", preco: "40", descricao: "descricao", imagem: "/chocolate.png"}
+]
+
+
+
 app.get('/', (req, res) => {
   res.render('index', { message: 'Ola' });
 });
